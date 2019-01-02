34e66ce0980439c58dd5a57bd298099101f95fae
chessai <chessai1996@gmail.com>
2019-01-02 17:23:52 -0500

    show

--- src/Hedgehog/Classes/Common.hs
+++ src/Hedgehog/Classes/Common.hs
@@ -3,7 +3,7 @@ module Hedgehog.Classes.Common
   
   , hLessThan, hGreaterThan
 
-  , genSmallList
+  , genSmallList, genShowReadPrecedences, genSmallString
   ) where
 
 import Hedgehog
@@ -19,6 +19,17 @@ data Laws = Laws
 
 genSmallList :: Gen a -> Gen [a]
 genSmallList gen = Gen.list (Range.linear 0 6) gen
+
+genSmallString :: Gen String
+genSmallString = Gen.string (Range.linear 0 10) Gen.ascii
+
+-- Haskell uses the operator precedences 0..9, the special function application
+-- precedence 10 and the precedence 11 for function arguments. Both show and
+-- read instances have to accept this range. According to the Haskell Language
+-- Report, the output of derived show instances in precedence context 11 has to
+-- be an atomic expression.
+genShowReadPrecedences :: Gen Int
+genShowReadPrecedences = Gen.element [0..11]
 
 -- | Fails the test if the right argument is less than or equal to the left.
 -- see https://github.com/hedgehogqa/haskell-hedgehog/pull/196 
---
+++ src/Hedgehog/Classes/Show.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Hedgehog.Classes.Show (showLaws) where
+
+import Hedgehog
+import Hedgehog.Classes.Common
+
+showLaws :: (Show a) => Gen a -> Laws
+showLaws gen = Laws "Show"
+  [ ("Show", showShowsPrecZero gen)
+  , ("Equivariance: showsPrec", equivarianceShowsPrec gen)
+  , ("Equivariance: showList", equivarianceShowList gen)
+  ]
+
+showShowsPrecZero :: forall a. (Show a) => Gen a -> Property
+showShowsPrecZero gen = property $ do
+  a <- forAll gen
+  show a === showsPrec 0 a ""
+
+equivarianceShowsPrec :: forall a. (Show a) => Gen a -> Property
+equivarianceShowsPrec gen = property $ do
+  p <- forAll genShowReadPrecedences
+  a <- forAll gen
+  r <- forAll genSmallString
+  s <- forAll genSmallString
+  showsPrec p a r ++ s === showsPrec p a (r ++ s)
+
+equivarianceShowList :: forall a. (Show a) => Gen a -> Property
+equivarianceShowList gen = property $ do
+  as <- forAll $ genSmallList gen
+  r <- forAll genSmallString
+  s <- forAll genSmallString
+  showList as r ++ s === showList as (r ++ s)
