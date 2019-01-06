d2466b0c3624b5c9abde2c827eb3dd1697b4d6f4
chessai <chessai1996@gmail.com>
2019-01-06 13:33:49 -0500

    contravariant

---
+++ src/Hedgehog/Classes/Contravariant.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Hedgehog.Classes.Contravariant (contravariantLaws) where
+
+import Data.Functor.Contravariant (Contravariant(..))
+
+import Hedgehog
+import Hedgehog.Classes.Common
+
+contravariantLaws ::
+  ( Contravariant f
+  , forall x. Eq x => Eq (f x), forall x. Show x => Show (f x)
+  ) => (forall x. Gen x -> Gen (f x)) -> Laws
+contravariantLaws gen = Laws "Contravariant"
+  [ ("Identity", contravariantIdentity gen)
+  , ("Composition", contravariantComposition gen)
+  ]
+
+contravariantIdentity ::
+  ( Contravariant f
+  , forall x. Eq x => Eq (f x), forall x. Show x => Show (f x)
+  ) => (forall x. Gen x -> Gen (f x)) -> Property
+contravariantIdentity fgen = property $ do
+  a <- forAll $ fgen genSmallInteger
+  contramap id a === id a
+
+contravariantComposition ::
+  ( Contravariant f
+  , forall x. Eq x => Eq (f x), forall x. Show x => Show (f x)
+  ) => (forall x. Gen x -> Gen (f x)) -> Property
+contravariantComposition fgen = property $ do
+  a <- forAll $ fgen genSmallInteger
+  f' <- forAll genQuadraticEquation
+  g' <- forAll genQuadraticEquation
+  let f = runQuadraticEquation f'
+  let g = runQuadraticEquation g'
+  (contramap f (contramap g a)) === (contramap (g . f) a)
