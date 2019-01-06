725fdea06646053d2dc617dac1590fbc3768fe2a
chessai <chessai1996@gmail.com>
2019-01-06 00:03:46 -0500

    more internal stuff, cabal updated

--- src/Hedgehog/Classes/Common.hs
+++ src/Hedgehog/Classes/Common.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Hedgehog.Classes.Common
   ( Laws(..)
@@ -14,19 +15,26 @@ module Hedgehog.Classes.Common
   , LinearEquation(..), runLinearEquation, genLinearEquation
   , LinearEquationM(..), runLinearEquationM, genLinearEquationM
   , LinearEquationTwo(..), runLinearEquationTwo, genLinearEquationTwo
+  , CubicEquation(..), runCubicEquation, genCubicEquation
 
-  , ChooseFirst(..), genChooseFirst
-  , ChooseSecond(..), genChooseSecond
-  , LastNothing(..), genLastNothing
   , Bottom(..), genBottom
 
-  , hackReplace
+  , Triple(..), reverseTriple, genTriple
 
-  , func1, func2, func3
+  , apTrans, toSpecialApplicative
+
+  , func1, func2, func3, func4, func5, func6
+  
+  , genCompose, genTuple, genSetInteger
   ) where
 
+import Data.Tuple (swap)
 import Hedgehog
 import GHC.Stack
+import Data.Functor.Classes (Eq1(..), Show1(..))
+import Data.Functor.Compose
+import qualified Data.Set as S
+import qualified Control.Monad.Trans.Writer.Lazy as WL
 import Data.Semigroup
 import Hedgehog.Internal.Property
 import qualified Hedgehog.Gen as Gen
@@ -158,13 +166,21 @@ genLinearEquationTwo = LinearEquationTwo <$> absGenInteger <*> absGenInteger <*>
 runLinearEquationTwo :: LinearEquationTwo -> Integer -> Integer -> Integer
 runLinearEquationTwo (LinearEquationTwo a b c) x y = a * x + b * y + c
 
-hackReplace :: (Functor f) => Gen b -> Gen (f a) -> Gen (f b)
-hackReplace genb genfa = do
-  b <- genb
-  fmap2 (const b) genfa
+data CubicEquation = CubicEquation
+  { _cubicEquationCubic :: Integer
+  , _cubicEquationQuadratic :: Integer
+  , _cubicEquationLinear :: Integer
+  , _cubicEquationConstant :: Integer
+  }
 
-fmap2 :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
-fmap2 = fmap . fmap
+instance Show CubicEquation where
+  show (CubicEquation x y z c) = "\\x -> " ++ show x ++ " * x ^ 3 + " ++ show y ++ " * x ^ 2 + " ++ show z ++ " * x + " ++ show c
+
+genCubicEquation :: Gen CubicEquation
+genCubicEquation = CubicEquation <$> genSmallInteger <*> genSmallInteger <*> genSmallInteger <*> genSmallInteger
+
+runCubicEquation :: CubicEquation -> Integer -> Integer -> Integer -> Integer
+runCubicEquation (CubicEquation a b c d) x y z = a * x + b * y + c * z + d
 
 func1 :: Integer -> (Integer, Integer)
 func1 i = (div (i + 5) 3, i * i - 2 * i + 1)
@@ -175,43 +191,94 @@ func2 (a,b) = (odd a, if even a then Left (compare a b) else Right (b + 2))
 func3 :: Integer -> Sum Integer
 func3 i = Sum (3 * i * i - 7 * i + 4)
 
-data ChooseFirst = ChooseFirst
-  deriving (Eq)
+func4 :: Integer -> Compose Triple (WL.Writer (S.Set Integer)) Integer
+func4 i = Compose $ Triple
+  (WL.writer (i * i, S.singleton (i * 7 + 5)))
+  (WL.writer (i + 2, S.singleton (i * i + 3)))
+  (WL.writer (i * 7, S.singleton 4))
 
-data ChooseSecond = ChooseSecond
-  deriving (Eq)
+func5 :: Integer -> Triple Integer
+func5 i = Triple (i + 2) (i * 3) (i * i)
 
-data LastNothing = LastNothing
-  deriving (Eq)
+func6 :: Integer -> Triple Integer
+func6 i = Triple (i * i * i) (4 * i - 7) (i * i * i)
 
+reverseTriple :: Triple a -> Triple a
+reverseTriple (Triple a b c) = Triple c b a
+
+data Triple a = Triple a a a
+  deriving (Show, Eq)
+
+instance Functor Triple where
+  fmap f (Triple a b c) = Triple (f a) (f b) (f c)
+
+instance Applicative Triple where
+  pure a = Triple a a a
+  Triple f g h <*> Triple a b c = Triple (f a) (g b) (h c)
+
+instance Foldable Triple where
+  foldMap f (Triple a b c) = f a <> f b <> f c
+
+instance Traversable Triple where
+  traverse f (Triple a b c) = Triple <$> f a <*> f b <*> f c
+
+tripleLiftEq :: (a -> b -> Bool) -> Triple a -> Triple b -> Bool
+tripleLiftEq p (Triple a1 b1 c1) (Triple a2 b2 c2) =
+  p a1 a2 && p b1 b2 && p c1 c2
+
+tripleLiftShowsPrec :: (Int -> a -> ShowS) -> ([a] -> ShowS) -> Int -> Triple a -> ShowS
+tripleLiftShowsPrec elemShowsPrec _ p (Triple a b c) = showParen (p > 10)
+  $ showString "Triple "
+  . elemShowsPrec 11 a
+  . showString " "
+  . elemShowsPrec 11 b
+  . showString " "
+  . elemShowsPrec 11 c
+
+instance Eq1 Triple where
+  liftEq = tripleLiftEq
+
+instance Show1 Triple where
+  liftShowsPrec = tripleLiftShowsPrec
+
+genTriple :: Gen a -> Gen (Triple a)
+genTriple gen = Triple <$> gen <*> gen <*> gen
+  
 data Bottom a = BottomUndefined | BottomValue a
   deriving (Eq)
-
-instance Show ChooseFirst where
-  show ChooseFirst = "\\a b -> if even a then a else b"
-
-instance Show ChooseSecond where
-  show ChooseSecond = "\\a b -> if even b then a else b"
-
-instance Show LastNothing where
-  show LastNothing = "0"
 
 instance Show a => Show (Bottom a) where
   show = \case
     BottomUndefined -> "undefined"
     BottomValue a   -> show a
 
-genChooseFirst :: Gen ChooseFirst
-genChooseFirst = pure ChooseFirst
-
-genChooseSecond :: Gen ChooseSecond
-genChooseSecond = pure ChooseSecond
-
-genLastNothing :: Gen LastNothing
-genLastNothing = pure LastNothing
-
 genBottom :: Gen a -> Gen (Bottom a)
 genBottom = fmap maybeToBottom . Gen.maybe
 
 maybeToBottom :: Maybe a -> Bottom a
 maybeToBottom = \case { Nothing -> BottomUndefined; Just a -> BottomValue a }
+
+-- Reverse the list and accumulate the writers. We
+-- cannot use Sum or Product or else it won't actually
+-- be a valid applicative transformation.
+apTrans ::
+     Compose Triple (WL.Writer (S.Set Integer)) a
+  -> Compose (WL.Writer (S.Set Integer)) Triple a
+apTrans (Compose xs) = Compose (sequenceA (reverseTriple xs))
+
+toSpecialApplicative ::
+     Compose Triple ((,) (S.Set Integer)) Integer
+  -> Compose Triple (WL.Writer (S.Set Integer)) Integer
+toSpecialApplicative (Compose (Triple a b c)) =
+  Compose (Triple (WL.writer (swap a)) (WL.writer (swap b)) (WL.writer (swap c)))
+
+genCompose :: forall f g a. Gen a -> (forall x. Gen x -> Gen (f x)) -> (forall x. Gen x -> Gen (g x)) -> Gen (Compose f g a)
+genCompose gen fgen ggen = Compose <$> fgen (ggen gen) 
+
+genTuple :: Gen a -> Gen b -> Gen (a,b)
+genTuple a b = (,) <$> a <*> b
+
+genSetInteger :: Gen (S.Set Integer)
+genSetInteger = do
+  xs <- sequence $ fmap (const genSmallInteger) [1..10 :: Integer]
+  pure $ foldMap S.singleton xs
