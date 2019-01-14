206dc8a0bc32e6d54cab848140b0cb3c9812742b
chessai <chessai1996@gmail.com>
2019-01-14 11:35:45 -0500

    export genericLaws

--- src/Hedgehog/Classes/Generic.hs
+++ src/Hedgehog/Classes/Generic.hs
@@ -4,13 +4,12 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 
-module Hedgehog.Classes.Generic (genericLaws, generic1Laws) where
+module Hedgehog.Classes.Generic (genericLaws) where
 
 import Hedgehog
 import Hedgehog.Classes.Common
 
-import GHC.Generics (Generic(..), Generic1(..))
-import qualified Hedgehog.Gen as Gen
+import GHC.Generics (Generic(..))
 
 genericLaws ::
   ( Generic a, Eq a, Show a
@@ -22,20 +21,6 @@ genericLaws ::
 genericLaws gena genr = Laws "Generic"
   [ ("From-To inverse", fromToInverse gena genr)
   , ("To-From inverse", toFromInverse gena genr) 
-  ]
-
-generic1Laws ::
-  ( Generic1 f
-  , forall x. Eq x => Eq (f x)
-  , forall x. Show x => Show (f x)
-  , forall x. Eq x => Eq (Rep1 f x)
-  , forall x. Show x => Show (Rep1 f x)
-  ) => (forall x. Gen x -> Gen (f x))
-    -> (forall x. Gen x -> Gen (Rep1 f x))
-    -> Laws
-generic1Laws genf genr = Laws "Generic1"
-  [ ("From1-To1 inverse", fromToInverse1 genf genr)
-  , ("To1-From1 inverse", toFromInverse1 genf genr)
   ]
 
 fromToInverse :: forall a x.
@@ -56,6 +41,7 @@ toFromInverse gena _genr = property $ do
   v <- forAll gena
   to (from v) === v
 
+{-
 type Generic1Prop f =
   ( Generic1 f
   , forall x. Eq x => Eq (f x)
@@ -75,4 +61,20 @@ toFromInverse1 :: forall f. Generic1Prop f
 toFromInverse1 genf _genr = property $ do
   v <- forAll $ genf genSmallInteger
   v === (to1 . from1 $ v)
+-}
 
+{-
+generic1Laws ::
+  ( Generic1 f
+  , forall x. Eq x => Eq (f x)
+  , forall x. Show x => Show (f x)
+  , forall x. Eq x => Eq (Rep1 f x)
+  , forall x. Show x => Show (Rep1 f x)
+  ) => (forall x. Gen x -> Gen (f x))
+    -> (forall x. Gen x -> Gen (Rep1 f x))
+    -> Laws
+generic1Laws genf genr = Laws "Generic1"
+  [ ("From1-To1 inverse", fromToInverse1 genf genr)
+  , ("To1-From1 inverse", toFromInverse1 genf genr)
+  ]
+-}
