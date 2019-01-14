222eb94061e2cede0addf3a05bfb74890ba62125
chessai <chessai1996@gmail.com>
2019-01-14 11:34:53 -0500

    finish semiringlaws, add ringlaws

--- src/Hedgehog/Classes/Semiring.hs
+++ src/Hedgehog/Classes/Semiring.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
 
-module Hedgehog.Classes.Semiring (semiringLaws) where
+module Hedgehog.Classes.Semiring (semiringLaws, ringLaws) where
 
 import Hedgehog
 import Hedgehog.Classes.Common
@@ -10,10 +11,71 @@ import Data.Semiring
 
 semiringLaws :: (Semiring a, Eq a, Show a) => Gen a -> Laws
 semiringLaws gen = Laws "Semiring"
-  [
+  [ ("Additive Left Identity", semiringAdditiveLeftIdentity gen)
+  , ("Additive Right Identity", semiringAdditiveRightIdentity gen)
+  , ("Additive Associativity", semiringAdditiveAssociativity gen)
+  , ("Additive Commutativity", semiringAdditiveCommutativity gen)
+  , ("Multiplicative Left Identity", semiringMultiplicativeLeftIdentity gen)
+  , ("Multiplicative Right Identity", semiringMultiplicativeRightIdentity gen)
+  , ("Multiplicative Associativity", semiringMultiplicativeAssociativity gen)
+  , ("Multiplication Left-Distributes Over Addition", semiringLeftMultiplicationDistributes gen)
+  , ("Multiplication Right-Distributes Over Addition", semiringRightMultiplicationDistributes gen)
+  , ("Left Annihilation", semiringLeftAnnihilation gen) 
+  , ("Right Annihilation", semiringRightAnnihilation gen) 
+  ]
+
+ringLaws :: (Ring a, Eq a, Show a) => Gen a -> Laws
+ringLaws gen = Laws "Ring"
+  [ ("Additive Inverse", ringAdditiveInverse gen)
   ]
 
 type SemiringProp a = (Semiring a, Eq a, Show a) => Gen a -> Property
+type RingProp a = (Ring a, Eq a, Show a) => Gen a -> Property
+
+ringAdditiveInverse :: forall a. RingProp a
+ringAdditiveInverse gen = property $ do
+  a <- forAll gen
+  negate a + a === zero
+
+semiringAdditiveLeftIdentity :: forall a. SemiringProp a
+semiringAdditiveLeftIdentity gen = property $ do
+  x <- forAll gen
+  zero + x === x
+
+semiringAdditiveRightIdentity :: forall a. SemiringProp a
+semiringAdditiveRightIdentity gen = property $ do
+  x <- forAll gen
+  x + zero === x
+
+semiringAdditiveAssociativity :: forall a. SemiringProp a
+semiringAdditiveAssociativity gen = property $ do
+  a <- forAll gen
+  b <- forAll gen
+  c <- forAll gen
+  a + (b + c) === (a + b) + c
+
+semiringAdditiveCommutativity :: forall a. SemiringProp a
+semiringAdditiveCommutativity gen = property $ do
+  a <- forAll gen
+  b <- forAll gen
+  a + b === b + a
+
+semiringMultiplicativeLeftIdentity :: forall a. SemiringProp a
+semiringMultiplicativeLeftIdentity gen = property $ do
+  x <- forAll gen
+  one * x === x
+
+semiringMultiplicativeRightIdentity :: forall a. SemiringProp a
+semiringMultiplicativeRightIdentity gen = property $ do
+  x <- forAll gen
+  x * one === x 
+
+semiringMultiplicativeAssociativity :: forall a. SemiringProp a
+semiringMultiplicativeAssociativity gen = property $ do
+  a <- forAll gen
+  b <- forAll gen
+  c <- forAll gen
+  a * (b * c) === (a * b) * c
 
 semiringLeftMultiplicationDistributes :: forall a. SemiringProp a
 semiringLeftMultiplicationDistributes gen = property $ do
@@ -21,30 +83,20 @@ semiringLeftMultiplicationDistributes gen = property $ do
   b <- forAll gen
   c <- forAll gen
   (a * (b + c)) === ((a * b) + (a * c))
- 
+
 semiringRightMultiplicationDistributes :: forall a. SemiringProp a
 semiringRightMultiplicationDistributes gen = property $ do
-  ((a + b) * c) === 
-semiringLeftIdentityPlus :: forall a. SemiringProp a
-semiringLeftIdentityPlus gen = property $ do
-
-semiringRightIdentityPlus :: forall a. SemiringProp a
-semiringRightIdentityPlus gen = property $ do
-
-semiringLeftIdentityTimes :: forall a. SemiringProp a
-semiringLeftIdentityTimes gen = property $ do
-
-semiringRightIdentityTimes :: forall a. SemiringProp a
-semiringRightIdentityTimes gen = property $ do
+  a <- forAll gen
+  b <- forAll gen
+  c <- forAll gen
+  (a + b) * c === (a * c) + (b * c)
 
 semiringLeftAnnihilation :: forall a. SemiringProp a
 semiringLeftAnnihilation gen = property $ do
+  x <- forAll gen
+  zero * x === x
 
 semiringRightAnnihilation :: forall a. SemiringProp a
 semiringRightAnnihilation gen = property $ do
-
-semiringCommutativePlus :: forall a. SemiringProp a
-semiringCommutativePlus gen = property $ do
-
-semiringAssociativeTimes :: forall a. SemiringProp a
-semiringAssociativeTimes gen = property $ do
+  x <- forAll gen
+  x * zero === x
