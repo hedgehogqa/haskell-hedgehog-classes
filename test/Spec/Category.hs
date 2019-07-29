0a9514cd599b7c6191e2c587e4f88f0e1bfb56bd
chessai <chessai1996@gmail.com>
2019-07-29 12:28:43 -0400

    add dummy category to test suite to make me feel better about myself

--- test/Spec/Category.hs
+++ test/Spec/Category.hs
@@ -1,9 +1,26 @@
 module Spec.Category (testCategory, testCommutativeCategory) where
 
+import Control.Category
+import Hedgehog
 import Hedgehog.Classes
+import Prelude hiding ((.), id)
 
 testCategory :: [(String, [Laws])]
-testCategory = []
+testCategory =
+  [ ("ProxyC", [categoryLaws genProxyC])
+  ]
 
 testCommutativeCategory :: [(String, [Laws])]
-testCommutativeCategory = []
+testCommutativeCategory =
+  [ ("ProxyC", [commutativeCategoryLaws genProxyC])
+  ]
+
+data ProxyC a b = ProxyC
+  deriving (Eq, Show)
+
+instance Category ProxyC where
+  id = ProxyC
+  _ . _ = ProxyC
+
+genProxyC :: Gen a -> Gen b -> Gen (ProxyC a b)
+genProxyC _ _ = pure ProxyC
