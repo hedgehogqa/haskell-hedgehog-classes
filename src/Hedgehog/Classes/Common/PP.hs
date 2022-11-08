bce683da8306cf6e643b0d432cb0c19fda5a72d4
Ollie Charles <ollie@ocharles.org.uk>
2022-11-08 09:35:08 +0000

    Build with hedgehog-1.2

--- src/Hedgehog/Classes/Common/PP.hs
+++ src/Hedgehog/Classes/Common/PP.hs
@@ -35,8 +35,8 @@ renderResult x = renderDoc u =<< ppResult x
 ppResult :: MonadIO m
   => Report Result
   -> m (Doc Markup)
-ppResult r@(Report tests discards coverage status) = case status of
-  Failed (FailureReport size seed shrinks _mcoverage annots _mspan msg _mdiff footnotes) ->
-    let failure = Failed $ FailureReport size seed shrinks Nothing annots Nothing msg Nothing footnotes
-    in R.ppResult Nothing (Report tests discards coverage failure)
+ppResult r@(Report tests discards coverage seed status) = case status of
+  Failed (FailureReport shrinks shrinkPath _mcoverage annots _mspan msg _mdiff footnotes) ->
+    let failure = Failed $ FailureReport shrinks shrinkPath Nothing annots Nothing msg Nothing footnotes
+    in R.ppResult Nothing (Report tests discards coverage seed failure)
   _ -> R.ppResult Nothing r
